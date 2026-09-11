e08c2cd7b96af54b607d7d3dfb1baf74f4b1d8c9
Daniel Godoy <danielgodoy66@gmail.com>
2026-09-11 23:32:38 +0200

    feat: add create user endpoint

---
+++ src/app/api/users/route.ts
@@ -0,0 +1,40 @@
+import { prisma } from "@/lib/prisma";
+import { NextResponse } from "next/server";
+
+export async function POST(request: Request) {
+  try {
+
+    const body = await request.json();
+    const { username, email } = body;
+
+    // Check if recieved data is ok
+    if (!username || !email) {
+      return NextResponse.json(
+        { error: "Username and email are mandatory" },
+        { status: 400 }
+      );
+    }
+    // Check if user or email already exist
+    const existingUser = await prisma.user.findFirst({
+      where: { OR: [{ username }, { email }], },
+    });
+    if (existingUser) {
+      return NextResponse.json(
+        { error: "Username or email already exist" },
+        { status: 409 }
+      );
+    }
+    // Create user
+    const user = await prisma.user.create({
+      data: { username, email, },
+    });
+    return NextResponse.json(user, { status: 201 });
+
+  } catch (err) {
+    console.error(err);
+    return NextResponse.json(
+      { error: "Internal error from server" },
+      { status: 500 }
+    );
+  }
+}
